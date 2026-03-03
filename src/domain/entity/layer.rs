af984d9badc546dcf9bfbf41edfd0a3d20ce77ab
makinzm <nozomi.maki.da@gmail.com>
2026-03-03 14:22:21 +0900

    [test] RED: setup mille.toml parsing test and domain entities because of reading config specifications

---
+++ src/domain/entity/layer.rs
@@ -0,0 +1,32 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, PartialEq, Eq)]
+pub struct LayerConfig {
+    pub name: String,
+    pub paths: Vec<String>,
+    pub dependency_mode: DependencyMode,
+    #[serde(default)]
+    pub allow: Vec<String>,
+    #[serde(default)]
+    pub deny: Vec<String>,
+    pub external_mode: DependencyMode,
+    #[serde(default)]
+    pub external_allow: Vec<String>,
+    #[serde(default)]
+    pub external_deny: Vec<String>,
+    #[serde(default)]
+    pub allow_call_patterns: Vec<CallPattern>,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq, Clone, Copy)]
+#[serde(rename_all = "kebab-case")]
+pub enum DependencyMode {
+    OptIn,
+    OptOut,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq)]
+pub struct CallPattern {
+    pub callee_layer: String,
+    pub allow_methods: Vec<String>,
+}
